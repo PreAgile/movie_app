9059c4e6739c393acaac32484171544c07a913f2
PreAgile <>
2020-11-20 21:34:54 +0900

    fetching Movies from API

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  getMovies = async () => {
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+  };
+  componentDidMount() {
+    let { isLoading } = this.state;
+    this.getMovies();
+    isLoading = false;
+  }
+  state = {
+    isLoading: true,
+  };
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'READY!'}</div>;
+  }
 }
 
 export default App;
